91642fe5a1d894ed91e7f0c57215c8fa6bbd91e2
yungyuhun <okk7381@gmail.com>
2025-07-13 22:22:07 +0900

    lenis 패키지 재설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-import Lenis from "@studio-freight/lenis";
+import Lenis from 'lenis'
 import Splash from "@/components/Splash";
 import "../styles/globals.css";
 import "swiper/css";
